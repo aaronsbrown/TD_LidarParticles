f9fdcbd489e29583f28ebb84cab86f00d97cc8c7
Aaron Brown <aaron.s.brown@gmail.com>
2024-07-10 11:06:15 -0700

    pushing to github

--- scripts/lidarParticlesExt.py
+++ scripts/lidarParticlesExt.py
@@ -1,6 +1,8 @@
 from TDStoreTools import StorageManager
 import TDFunctions as TDF
 
+_PANELS = [op.PARTICLES]
+
 class lidarParticlesExt:
 	"""
 	lidarParticlesExt description
@@ -8,7 +10,8 @@ class lidarParticlesExt:
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
-
+		self.panels = _PANELS
+	
 	def findExternalOps(self):
 		tox = op.PROJ.findChildren(type=COMP)
 		external = [ t for t in tox if t.par.externaltox != '' ]
@@ -18,3 +21,9 @@ class lidarParticlesExt:
 		ext = self.findExternalOps()
 		for op in ext:
 			op.save(op.par.externaltox.eval())
+
+	def SimulateClick(self, u, v):
+		for panel in self.panels:
+			panel.click(u, v)
+			print(f'clicked {panel.name} at ({u},{v})')
+  
